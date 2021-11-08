0cc03fc01efa09b63ac9670677cf07033514327f
sidd-oo <sahoosiddharth3@gmail.com>
2021-11-08 11:34:13 +0530

    Featured component v2 - Series/movie title and Select genre options

--- src/components/featured/Featured.jsx
+++ src/components/featured/Featured.jsx
@@ -3,9 +3,31 @@ import './Featured.scss'
 import PlayCircleFilledWhiteOutlinedIcon from '@mui/icons-material/PlayCircleFilledWhiteOutlined';
 import { InfoOutlined } from '@mui/icons-material';
 
-const Featured = () => {
+const Featured = (props) => {
     return (
         <div class = "featured">
+
+        { props.type && (
+            <div className="category">
+                <span>{props.type === "movie" ? "Movies" : "Series"}</span>
+                <select name="genre" id="genre">
+                    <option>Genre</option>
+                    <option value="adventure">Adventure</option>
+                    <option value="comedy">Comedy</option>
+                    <option value="crime">Crime</option>
+                    <option value="fantasy">Fantasy</option>
+                    <option value="historical">Historical</option>
+                    <option value="horror">Horror</option>
+                    <option value="romance">Romance</option>
+                    <option value="sci-fi">Sci-fi</option>
+                    <option value="thriller">Thriller</option>
+                    <option value="western">Western</option>
+                    <option value="animation">Animation</option>
+                    <option value="drama">Drama</option>
+                    <option value="documentary">Documentary</option>
+                </select>
+            </div>
+        )}
            <img
                 src="https://images.pexels.com/photos/6899260/pexels-photo-6899260.jpeg?auto=compress&cs=tinysrgb&dpr=2&w=500"
                 alt=""
