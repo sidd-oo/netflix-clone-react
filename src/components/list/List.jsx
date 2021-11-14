ba45178abc2557b8505defee4ec7e29ffc1e4432
sidd-oo <sahoosiddharth3@gmail.com>
2021-11-14 20:33:34 +0530

    Added movements for sliderWindow using isMoved and slideNumber

--- src/components/list/List.jsx
+++ src/components/list/List.jsx
@@ -5,34 +5,33 @@ import ArrowForwardIosSharpIcon from '@mui/icons-material/ArrowForwardIosSharp';
 import ListItem from '../listItem/ListItem';
 
 const List = () => {
-    //
-    // const [isMoved, setIsMoved] = useState(false);
-    // const [slideNumber, setSlideNumber] = useState(0);
+    const [isMoved, setIsMoved] = useState(false);
+    const [slideNumber, setSlideNumber] = useState(0);
 
     const listRef = useRef();
 
     const handleClick = (direction) => {
-        // setIsMoved(true);
-        let distance = listRef.current.getBoundingClientRect();
+        setIsMoved(true)
+
+        let distance = listRef.current.getBoundingClientRect().x - 50;
         console.log(distance);
 
-        if (direction === "left") {
-            // setSlideNumber(slideNumber - 1);
-            listRef.current.style.transform = `translateX(${230}px)`;
+        if (direction === "left" && slideNumber > 0) {
+            setSlideNumber(slideNumber - 1);
+            listRef.current.style.transform = `translateX(${230 + distance}px)`;
         }
-        // if (direction === "right" && slideNumber < 5) {
-        //     setSlideNumber(slideNumber + 1);
-        //     listRef.current.style.transform = `translateX(${-230 + distance}px)`;
-        // }
+        if (direction === "right" && slideNumber < 5) {
+            setSlideNumber(slideNumber + 1);
+            listRef.current.style.transform = `translateX(${-230 + distance}px)`;
+        }
     };
-    //
 
     return (        
         <div className="list">
             <span className="listTitle">Continue to watch</span>
             <div className="wrapper">
                 <ArrowBackIosNewSharpIcon  className="sliderArrow left" onClick = {() => handleClick("left")}
-                // style = {{ display: !isMoved && "none" }}
+                style = {{ display: !(isMoved && (slideNumber !== 0)) && "none" }}
                 />
                 <div className="container" ref = {listRef}>
                     <ListItem/>
