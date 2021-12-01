08c656a1ca167cdd1cf529b3ced5d327a97aba0d
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-01 12:33:11 +0530

    feat: REST API for Registration

--- api/index.js
+++ api/index.js
@@ -2,12 +2,17 @@ const express = require('express');
 const app = express();
 const mongoose = require('mongoose');
 const dotenv = require('dotenv');
+const authRoute = require('./routes/auth')
 
 dotenv.config();
 
 mongoose.connect(process.env.MONGO_URL)
     .then(()=> console.log("DB Connected Successfully!"))
-    .catch((err) => console.log(err))
+    .catch((err) => console.log(err));
+
+app.use(express.json());
+
+app.use("/api/auth", authRoute);
 
 app.listen(8800, ()=>{
     console.log("Backend server is running...");
---
+++ api/routes/auth.js
@@ -0,0 +1,22 @@
+const router = require('express').Router();
+const User = require('../models/User');
+const CryptoJS = require('crypto-js');
+
+//REGISTER
+
+router.post("/register", async (req, res) => {
+    const newUser = new User({
+        username: req.body.username,
+        email: req.body.email,
+        password: CryptoJS.AES.encrypt(req.body.password, process.env.SECRET_KEY).toString()
+    });
+
+    try{
+        const user = await newUser.save();
+        res.status(201).json(user); 
+    }catch(err){
+        res.status(500).json(err);
+    }
+});
+
+module.exports = router;
