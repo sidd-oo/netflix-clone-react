2388b80e770ed4c20fe1e0769b085c3a095688fe
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-02 19:53:49 +0530

    fix: Error handling for Invalid tokens

--- api/verifyToken.js
+++ api/verifyToken.js
@@ -2,15 +2,18 @@ const jwt = require('jsonwebtoken');
 
 function verify(req, res, next){
     const authHeader = req.headers.token;
+
     if(authHeader){
         const token = authHeader.split(" ")[1];
-
-        jwt.verify(token, process.env.SECRET_KET, (err, user) => {
-            if(err) res.status(403).json("Token is not valid!");
+        
+        jwt.verify(token, process.env.SECRET_KEY, (err, user) => {
+            if(err) return res.status(403).json("Token is not valid!");
             req.user = user;
             next();
         });
+
     }else{
         return res.status(401).json("You are not authenticated!");
     }
-}
+} 
+module.exports = verify
