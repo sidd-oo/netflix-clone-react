6e48b44fad2078c239b7f411e6b8502a321387d7
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-02 16:05:15 +0530

    feat: REST API for Login with JWT Authentication

--- api/routes/auth.js
+++ api/routes/auth.js
@@ -1,6 +1,7 @@
 const router = require('express').Router();
 const User = require('../models/User');
 const CryptoJS = require('crypto-js');
+const jwt = require('jsonwebtoken');
 
 //REGISTER
 
@@ -19,4 +20,29 @@ router.post("/register", async (req, res) => {
     }
 });
 
+//LOGIN
+
+router.post("/login", async(req, res) => {
+    try{
+        const user = await User.findOne({email: req.body.email});
+        !user && res.status(401).json("Wrong password or username")
+
+        const bytes = CryptoJS.AES.decrypt(user.password, process.env.SECRET_KEY);
+        const originalPassword = bytes.toString(CryptoJS.enc.Utf8);
+
+        originalPassword !== req.body.password && 
+            res.status(401).json("Wrong password or username");
+
+        const accessToken = jwt.sign(
+            { id:user._id, isAdmin: user.isAdmin }, 
+            process.env.SECRET_KEY, 
+            {expiresIn: "5d"}
+        );
+        const { password, ...info} = user._doc;
+        
+        res.status(200).json({...info, accessToken});
+    }catch(err){
+        res.status(500).json(err);
+    }
+})
 module.exports = router;
