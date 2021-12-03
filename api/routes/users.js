06dad53577ea1433c21dbdf8041c09d7f2a10d73
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-03 17:23:03 +0530

    feat: REST API user CRUD operations

--- api/routes/users.js
+++ api/routes/users.js
@@ -6,7 +6,7 @@ const verify = require('../verifyToken');
 //UPDATE
 
 router.put("/:id", verify , async (req, res) => {
-    if(req.user.id === req.params.id || req.user.isAdmin){
+    if(req.user. id === req.params.id || req.user.isAdmin){
         if(req.body.password){
             req.body.password = CryptoJS.AES.encrypt(
                 req.body.password,
@@ -28,14 +28,76 @@ router.put("/:id", verify , async (req, res) => {
     }else{
         res.status(403).json("You can update only your account!");
     }
-})
+});
 
 //DELETE
 
+router.delete("/:id", verify , async (req, res) => {
+    if(req.user.id === req.params.id || req.user.isAdmin){
+        try {
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json("User has been deleted");
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json("You can delete only your account!");
+    }
+});
+
 //GET
+
+router.get("find/:id", verify , async (req, res) => {
+        try {
+            const user = await User.findById(req.params.id);
+            const {password, ...info} = user._doc;
+            res.status(200).json(info);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+});
 
 //GET ALL
 
+router.get("/", verify , async (req, res) => {
+    const query = req.query.new;
+    if(req.user.isAdmin){
+        try {
+            const users = query ? await User.find().sort({_id:-1}).limit(10) : await User.find();
+            await User.findByIdAndDelete(req.params.id);
+            res.status(200).json(users);
+        } catch (err) {
+            res.status(500).json(err);
+        }
+    }else{
+        res.status(403).json("You are not allowed to see all users!");
+    }
+});
+
 //GET USER STATS
+
+router.get("/stats", async (req, res) => {
+  const today = new Date();
+  const lastYear = today.setFullYear(today.setFullYear() - 1);
+
+  try {
+    const data = await User.aggregate([
+      {
+        $project: {
+          month: { $month: "$createdAt" },
+        },
+      }, 
+      {
+        $group: {
+          _id: "$month",
+          total: { $sum: 1 },
+        },
+      },
+    ]);
+    res.status(200).json(data)
+  } catch (err) {
+    res.status(500).json(err);
+  }
+});
 
 module.exports = router;
