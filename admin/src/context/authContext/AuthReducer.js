bf7d94c0ac41a5fe46d308064d97f58d416dffd6
sidd-oo <sahoosiddharth3@gmail.com>
2021-12-30 07:19:34 +0530

    style: Adding missing double quotes

--- admin/src/context/authContext/AuthReducer.js
+++ admin/src/context/authContext/AuthReducer.js
@@ -1,18 +1,18 @@
 const AuthReducer = (state, action) => {
     switch(action.type){
-        case LOGIN_START: 
+        case "LOGIN_START": 
             return {
                 user: null,
                 isFetching: true,
                 error: false
             };
-        case LOGIN_SUCCESS:
+        case "LOGIN_SUCCESS":
             return {
                 user: action.payload,
                 isFetching: false,
                 error: false
             };
-        case LOGIC_FAILURE:
+        case "LOGIC_FAILURE":
             return {
                 user: null,
                 isFetching: false,
