b642c6671f8022e2dea5c72161a705d72e2bc690
sidd-oo <sahoosiddharth3@gmail.com>
2022-01-15 11:44:35 +0530

    fix: Redirected all the * routes of the application to certain pages

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,48 +1,55 @@
 import React, { useContext } from "react";
 import {
-  BrowserRouter as Router,
-  Routes,
-  Route,
-  Navigate,
+  BrowserRouter as Router, Navigate, Route, Routes
 } from "react-router-dom";
-import Login from "./pages/login/Login";
-import Register from "./pages/register/Register";
+import { AuthContext } from "./context/authContext/AuthContext";
 import Home from "./pages/Home/Home";
-import Watch from "./pages/watch/Watch";
-import { AuthContext} from "./context/authContext/AuthContext";
-import NewAndPopular from "./pages/NewAndPopular/NewAndPopular";
+import Login from "./pages/login/Login";
 import MyList from "./pages/MyList/MyList";
+import NewAndPopular from "./pages/NewAndPopular/NewAndPopular";
+import Register from "./pages/register/Register";
+import Watch from "./pages/watch/Watch";
 
 const App = () => {
-  const {user} = useContext(AuthContext);
+  const { user } = useContext(AuthContext);
 
   return (
-      <Router>
-        <Routes>
-          <Route
-            exact
-            path="/"
-            element={user ? <Home /> : <Navigate to="/register" />}
-          />
-          <Route
-            path="/register"
-            element={!user ? <Register /> : <Navigate to="/" />}
-          />
-          <Route
-            path="/login"
-            element={!user ? <Login /> : <Navigate to="/" />}
-          ></Route>
-          {user && (
-            <>
-              <Route path="/movies" element={<Home type="movie" />} />
-              <Route path="/series" element={<Home type="serie" />} />
-              <Route path="/watch" element={<Watch />} />
-              <Route path="/newPopular" element={<NewAndPopular/>}/>
-              <Route path="/myList" element={<MyList/>}/>
-            </>
-          )}
-        </Routes>
-      </Router>
+    <Router>
+      <Routes>
+        <Route
+          exact
+          path="/"
+          element={user ? <Home /> : <Navigate to="/register" />}
+        />
+        <Route
+          path="/register"
+          element={!user ? <Register /> : <Navigate to="/" />}
+        />
+        <Route
+          path="/login"
+          element={!user ? <Login /> : <Navigate to="/" />}
+        ></Route>
+        {user ? (
+          <>
+            <Route path="/movies" element={<Home type="movie" />} />
+            <Route path="/series" element={<Home type="serie" />} />
+            <Route path="/watch" element={<Watch />} />
+            <Route path="/newPopular" element={<NewAndPopular />} />
+            <Route path="/myList" element={<MyList />} />
+            <Route path="*"  element={<Navigate to="/" />} />
+          </>
+        ) : (
+          <>
+            <Route path="/movies" element={<Login/>} />
+            <Route path="/series" element={<Login/>} />
+            <Route path="/watch" element={<Login/>} />
+            <Route path="/newPopular" element={<Login />} />
+            <Route path="/myList" element={<Login />} />
+            <Route path="*" exact = {true} element={<Navigate to="/login" />} />
+          </>
+        )}
+      </Routes>
+    </Router>
   );
 };
 
